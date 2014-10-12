f9dcdef35c43527ba1a8b9e7b741e86014f380ea
Israel Loredo <israelloredom@gmail.com>
2014-10-12 23:57:20 +0200

    Added proyect commons

---
+++ Commons/Commons/Input.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Commons
+{
+    public class Input
+    {
+        public static int numberInt(String message, string errorMessage)
+        {
+            int number = 0;
+            bool valid = false;
+            string input;
+
+            do
+            {
+                Console.WriteLine(message);
+                input = Console.ReadLine();
+                valid = int.TryParse(input, out number);
+                if (!valid)
+                {
+                    Console.WriteLine(errorMessage);
+                }
+            }
+            while (!valid);
+
+            return number;
+        }
+    }
+}
